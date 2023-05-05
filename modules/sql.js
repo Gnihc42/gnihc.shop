905b60254bf34b72f30e5f0b838bcc54e1c6fe83
Gnihc <meeh69420@gmail.com>
2023-05-05 09:34:58 +0700

    'l'

--- modules/sql.js
+++ modules/sql.js
@@ -12,8 +12,8 @@ async function openPool(){
     port:  "5433",
     connectionString: process.env.sqlStr, 
   });
-  print(process.env.sqlPass)
-  print(process.env.sqlStr)
+  console.log(process.env.sqlPass)
+  console.log(process.env.sqlStr)
   query = util.promisify(pool.query).bind(pool);
   console.log(await query('SELECT * from banhang'));
 }
